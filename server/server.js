6682695371f1b1f715235996783349e223fa6b64
vongarcia97 <vongarcia97@gmail.com>
2023-02-02 03:25:53 -0500

    update links

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.resolve(__dirname, '../dist')));
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://bitepaynyc-env.eba-jjnppubt.us-east-1.elasticbeanstalk.com/",
+    origin: "http://bitepaynyc-env.eba-jjnppubt.us-east-1.elasticbeanstalk.com/",
   }
 });
 
